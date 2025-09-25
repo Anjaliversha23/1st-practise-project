0299d764c8e9226ecb64dce7c57a4cc6ef854331
Anjaliversha23 <anjaliversha23@outlook.com>
2025-09-25 10:42:44 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 name = str(input("Enter your name: "))
 age = int(input("Enter your age: "))
 gender = str(input("Eter your gender: "))
-print(f"Hello {name} welcome your age is {age} and your gender is {gender}")
+print(f"Hello {name} welcome your age is {age} and your gender is {gender}, all the best.")
