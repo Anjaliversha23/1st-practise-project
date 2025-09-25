4dfefef7fae049c095bc02d83ad51ccb1e5acf56
Anjaliversha23 <anjaliversha23@outlook.com>
2025-09-25 09:20:04 +0530

    first file

---
+++ main.py
@@ -0,0 +1,4 @@
+name = str(input("Enter your name: "))
+age = int(input("Enter your age: "))
+gender = str(input("Eter your gender: "))
+print(f"Hello {name} your age is {age} and your gender is {gender}")
